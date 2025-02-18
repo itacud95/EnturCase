6f80dce47bf80eea6873da0a2f217584b171d251
jk <jakobhkrohn@gmail.com>
2025-02-18 11:15:18 +0100

    add theme

--- app/src/main/java/com/example/enturcase/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/enturcase/ui/screen/HomeScreen.kt
@@ -1,38 +1,25 @@
 package com.example.enturcase.ui.screen
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.enturcase.data.model.StopPlace
 import com.example.enturcase.ui.components.StopPlaceItem
 import com.example.enturcase.ui.navigation.Screen
+import com.example.enturcase.ui.theme.EnturCaseTheme
 import com.example.enturcase.ui.viewmodel.MainViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -40,30 +27,32 @@ import com.example.enturcase.ui.viewmodel.MainViewModel
 fun HomeScreen(navController: NavController, viewModel: MainViewModel) {
     val stopPlaces by viewModel.data.collectAsState()
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("Stop Places") },
-                actions = {
-                    IconButton(onClick = {
+    EnturCaseTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Stop Places") },
+                    actions = {
+                        IconButton(onClick = {
 //                        viewModel.refreshData()
-                    }) {
-                        Icon(imageVector = Icons.Default.Refresh, contentDescription = "Reload")
+                        }) {
+                            Icon(imageVector = Icons.Default.Refresh, contentDescription = "Reload")
+                        }
                     }
-                }
-            )
-        }
-    ) { paddingValues ->
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-                .padding(16.dp)
-        ) {
-            items(stopPlaces.size) {
-                val stopPlace = stopPlaces[it]
-                StopPlaceItem(stopPlace) {
-                    navController.navigate(Screen.Details.createRoute(stopPlace.name))
+                )
+            }
+        ) { paddingValues ->
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(16.dp)
+            ) {
+                items(stopPlaces.size) {
+                    val stopPlace = stopPlaces[it]
+                    StopPlaceItem(stopPlace) {
+                        navController.navigate(Screen.Details.createRoute(stopPlace.name))
+                    }
                 }
             }
         }
