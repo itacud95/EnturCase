db43c98f76da1924f0534b5ba0aa671360673029
jk <jakobhkrohn@gmail.com>
2025-02-19 17:01:19 +0100

    check for coarse or fine

--- app/src/main/java/com/example/enturcase/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/enturcase/ui/screen/HomeScreen.kt
@@ -4,41 +4,28 @@ import android.Manifest
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.enturcase.ui.components.StopPlaceItem
 import com.example.enturcase.ui.navigation.Screen
-import com.example.enturcase.ui.theme.EnturCaseTheme
 import com.example.enturcase.ui.viewmodel.LocationViewModel
-import com.example.enturcase.ui.viewmodel.NearbyStopsViewModel
 import com.example.enturcase.utils.Logger
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
-import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
 @Composable
 fun LocationPermissionDialog(viewModel: LocationViewModel) {
@@ -70,12 +57,19 @@ fun HomeScreen(
     navController: NavController,
     locationViewModel: LocationViewModel
 ) {
+    val locationPermissionsState = rememberMultiplePermissionsState(
+        listOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+    )
 
-    val locationPermissionState = rememberPermissionState(Manifest.permission.ACCESS_FINE_LOCATION)
+    val permissionsGranted = locationPermissionsState.permissions.any { it.status.isGranted }
 
-    LaunchedEffect(locationPermissionState.status) {
+
+    LaunchedEffect(permissionsGranted) {
         Logger.debug("HomeScreen LaunchedEffect")
-        if (locationPermissionState.status.isGranted) {
+        if (permissionsGranted) {
             navController.navigate(Screen.StopPlaces.route)
         }
     }
@@ -88,7 +82,7 @@ fun HomeScreen(
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onClick = { locationPermissionState.launchPermissionRequest() }) {
+        Button(onClick = { locationPermissionsState.launchMultiplePermissionRequest() }) {
             Text("Grant Permission")
         }
     }
